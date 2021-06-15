2286ad4c79b2ef6a34397b036bf0bfa593834fba
Andrii Serha <sergaav@gmail.com>
2021-06-15 16:25:53 +0300

    Initial commit

---
+++ src/test/java/com/savaz/deliveryspring/DeliverySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savaz.deliveryspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverySpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
